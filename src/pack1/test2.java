299be1f0e11c4ad9e76d10d80d954deb7c8fb94c
muhammet aydin <112701546+maydin61@users.noreply.github.com>
2022-12-10 18:30:42 +0200

    mine is over about task 1 , let me check it pls
    Ok

--- src/pack1/test2.java
+++ src/pack1/test2.java
@@ -2,5 +2,5 @@ package pack1;
 
 public class test2 {
   
-  // it is edited too, check the code format woorking well enough or not.
+  // it is edited too, check the code format woorking well enough or not. t
 }
